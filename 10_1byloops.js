96a2bd408adaa252a0f66f6348c08290b3ff93c3
Bhuvanesh037 <144247894+Bhuvanesh037@users.noreply.github.com>
2025-07-16 23:28:36 +0530

    Add files via upload

---
+++ 10_1byloops.js
@@ -0,0 +1,22 @@
+//10 to 1 by using for loop
+console.log("10 to 1 by using for loop")
+for(let i=10;i>=1;i--){
+    console.log(i)
+}
+
+////10 to 1 by using while loop
+console.log("10 to 1 by using while loop")
+let i1=10;
+while(i1>=1){
+    console.log(i1)
+    i1--
+}
+console.log("10 to 1 by using Do wile loop")
+
+
+let i2=10;
+do{
+    console.log(i2)
+    i2--
+}
+while(i2>=1)
---
+++ 1_10byloops.js
@@ -0,0 +1,22 @@
+//1 to 10 by using for loop
+console.log("1 to 10 by using for loop")
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+
+////1 to 10 by using while loop
+console.log("1 to 10 by using while loop")
+let i=1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+console.log("1 to 10 by using Do wile loop")
+let i2=1;
+do{
+console.log(i2);
+i2++;
+}
+while(i2<=10);
+
+
